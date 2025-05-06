d78ce61aee76d5718797a22854ae2f9079c98277
Abdulrahman <1xvlux1@gmail.com>
2025-05-06 13:18:57 +0300

    Update Lab4

--- lab4/scripts.js
+++ lab4/scripts.js
@@ -4,28 +4,26 @@ let saudiaAirlines = {
   fleetSize: 5,
   destinations: ["الرياض", "جدة", "دبي", "نيويورك"],
   inOperation: true,
+  bookFlight: function (destination) {
+    let search = this.destinations.find(function (value) {
+      return value === destination;
+    });
+    return search
+      ? `تم حجز الرحلة الى ${destination}`
+      : `عذرا، لا توجد رحلات الى ${destination}`;
+  },
+
+  addDestination: function (newDestination) {
+    this.destinations.push(newDestination);
+    console.log(saudiaAirlines);
+  },
+  getSummary: function () {
+    console.log(
+      `الخطوط الجوية السعودية تعمل في السعودية وتمتلك ${this.fleetSize} طائرة وتطير إلى ${this.destinations.length} وجهة.`
+    );
+  },
 };
 
-function bookFlight(destination) {
-  let search = saudiaAirlines.destinations.find(function (value) {
-    return value === destination;
-  });
-
-  return search
-    ? `تم حجز الرحلة الى ${destination}`
-    : `عذرا، لا توجد رحلات الى ${destination}`;
-}
-
-function addDestination(newDestination) {
-  saudiaAirlines.destinations.push(newDestination);
-  console.log(saudiaAirlines);
-}
-
-function getSummary() {
-  console.log(
-    `الخطوط الجوية السعودية تعمل في السعودية وتمتلك ${saudiaAirlines.fleetSize} طائرة وتطير إلى ${saudiaAirlines.destinations.length} وجهة.`
-  );
-}
 
 function sum(num1, num2) {
   return num1 + num2;
@@ -43,15 +41,13 @@ function div(num1, num2) {
   return num1 / num2;
 }
 
-console.log(bookFlight("الرياض"));
+console.log(saudiaAirlines.bookFlight("الرياض"));
 
-addDestination("دبي");
+saudiaAirlines.addDestination("دبي");
 
-getSummary();
+saudiaAirlines.getSummary();
 
-console.log(sum(1,2));
-console.log(sub(2,1));
-console.log(mult(3,1))
-console.log(div(10,2))
-
-
+console.log(sum(1, 2));
+console.log(sub(2, 1));
+console.log(mult(3, 1));
+console.log(div(10, 2));
